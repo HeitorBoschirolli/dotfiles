734e231c5eca19f774293d5d4505b4eb56f02c18
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2019-09-22 16:34:59 -0300

    update to oh-my-tmux config

---
+++ tmux/install.sh
@@ -0,0 +1,14 @@
+# tmux not installed: install it
+if ! hash tmux; then
+  sudo dnf install tmux
+fi
+
+# powerline fonts not installed: install it
+if ! hash powerline-fonts; then
+  sudo dnf install powerline-fonts
+fi
+
+# create soft link in home dir
+ln -s -f .tmux.conf ~/.tmux.conf
+cp .tmux.conf.local ~/
+
