fbc514fa2ae570347512b0a6f23124a63f4cc97f
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2019-09-22 18:27:25 -0300

    do stuff

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,13 +8,17 @@ fi
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# install powerline fonts
-sudo dnf install powerline-fonts
+# powerline fonts are not installed: install it
+if [ ! hash powerline-fonts 2>/dev/null ]; then
+  sudo dnf install powerline-fonts
+fi
 
-# install fzf
-sudo dnf install fzf
+# fzf is not installed: install it 
+if [ ! hash fzf 2>/dev/null ]; then
+  sudo dnf install fzf
+fi
 
 # install spaceship theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -f -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
