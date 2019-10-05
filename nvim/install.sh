2bb15d18366d94724b51ff713ce836e76f011a44
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2019-10-05 14:44:36 -0300

    Create init.vim if does not exist

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,4 +11,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create soft link in home dir
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
 ln -s -f $(pwd)/init.vim ~/.config/nvim/init.vim
